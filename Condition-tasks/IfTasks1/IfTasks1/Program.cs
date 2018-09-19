562a444ff03e62d405363687de5631890acf4a53
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-09-19 10:02:43 +0300

    If Task 1 done

--- Condition-tasks/IfTasks1/IfTasks1/Program.cs
+++ Condition-tasks/IfTasks1/IfTasks1/Program.cs
@@ -7,7 +7,18 @@ namespace IfTasks1
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla");
-            Console.WriteLine("Syötä luku");
+            Console.Write("Syötä luku: ");
+            string userInput =  Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if(number == 0)
+                Console.WriteLine($"Numero {number} on nolla");
+            else if(number<0)
+                Console.WriteLine($"Numero {number} on negatiivinen");
+            else
+                Console.WriteLine($"Numero {number} on positiivinen");
+
+            Console.WriteLine($"Syötit luvun {userInput}");
         }
     }
 }
