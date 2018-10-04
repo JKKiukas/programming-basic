709b4b81759ab216e963451f1c33024f7f6fc43d
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-04 08:20:12 +0300

    Loop task 3.2 done.

---
+++ Loop-task/LoopTask3.2/LoopTask3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopTask3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 0;
+            int luku = 0;
+            int i = 1;
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("Ohjelma tulostaa N! kertoman arvon.\n");
+
+            Console.WriteLine("Syötä luku. \n");
+            luku = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= luku; i++)
+                f = f + i;
+
+            if (luku < 1)
+                Console.WriteLine("Määrittelemätön.");
+            else
+                Console.WriteLine($"\nKertoma on {f}.");
+        }
+    }
+}
