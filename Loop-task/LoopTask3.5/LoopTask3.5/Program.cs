0154c30cbc1981146fb18058605d4e5e7647e613
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-04 11:13:42 +0300

    Loop task 3.5 done.

---
+++ Loop-task/LoopTask3.5/LoopTask3.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopTask3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int EvenSum = 0;
+            int OddSum = 0;
+            int i = 0;
+
+            Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen lukujen summan. \n");
+            Console.WriteLine("Syötä luku. \n");
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (i < number)
+                for (i = 1; i <= number; i++)
+                    if (i % 2 == 0)
+                        EvenSum = EvenSum + i;
+                    else
+                        OddSum = OddSum + i;
+            else
+                for (i = 0; i >= number; i--)
+                    if (i % 2 == 0)
+                        EvenSum = EvenSum + i;
+                    else
+                        OddSum = OddSum + i;
+
+            Console.WriteLine($"Parittomien summa on {OddSum} Parillisten summa on {EvenSum}");
+        }
+    }
+}
