32f84cefc22c639d55eced238ebc6bd5aa3c0595
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-16 14:23:09 +0300

    Loop task 4.5 done.

---
+++ Loop-task/LoopTask4.5/LoopTask4.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoopTask4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen rivin. \n");
+
+            Random roll = new Random();
+            double result;
+
+            for (int i = 1; i < 14; i++)
+            { result = roll.NextDouble();
+                if (result < 0.4)
+                    Console.WriteLine("Kotivoitto");
+                else if (result > 0.4 && result < 0.6)
+                    Console.WriteLine("Tasapeli");
+                else
+                    Console.WriteLine("Vierasvoitto");
+            }
+        }
+    }
+}
