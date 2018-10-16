88759fe6b122b912f63f6d90f09cdf7e10067f3f
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-16 14:47:34 +0300

    Loop task 4.6 done.

---
+++ Loop-task/LoopTask4.6/LoopTask4.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoopTask4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma heittää noppaa 1000 kertaa ja kertoo montako kertaa numero kuusi heitettiin. \n");
+
+            Random dice = new Random();
+            int result;
+            int count = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                result = dice.Next(6) + 1;
+                if (result == 6)
+                    count++;
+                Console.WriteLine($"{i + 1} {result}");
+            }
+            Console.WriteLine($"Numero kuusi arvottiin {count} kertaa");
+        }
+    }
+}
