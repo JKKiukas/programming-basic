e0662d05b1c293990d3dc87f2ca08b4ea35b735e
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-23 17:04:16 +0300

    Function task 3 done.

---
+++ Funktio-tasks/FunctionTask3/FunctionTask3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FunctionTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma pyytää käyttäjää syöttämään luvun väliltä 1-50. Jos syöte on virheellinen, lukua pyydetään uudestaan. \n");
+            int number = numberCompare(1, 50);
+            Console.WriteLine($"Syöttämäsi luku {number} on väliltä 1-50.");
+        }
+        static int numberCompare(int lower, int upper)
+        {
+            int permittedNumber;
+            while (true)
+            {
+                Console.WriteLine("Syötä luku: \n");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int userInput);
+                if (isNumber && userInput >= lower && userInput <= upper)
+                {
+                    permittedNumber = userInput;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Virheellinen syöte");
+                }
+            }
+            return permittedNumber;
+        }
+    }
+}
