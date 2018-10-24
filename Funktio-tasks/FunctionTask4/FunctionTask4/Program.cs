23c916c6e4bb6ff3b780294f1d96ecd86cbb0775
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-24 19:18:07 +0300

    Function task 4 done.

---
+++ Funktio-tasks/FunctionTask4/FunctionTask4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FunctionTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy käyttäjältä 10 positiivista kokonaislukua ja kertoo mikä niistä oli suurin. \n");
+
+            int j = 0;
+            Console.WriteLine($"Suurin luku oli {Numbers(ref j)} ja se on rivillä {j}.\n");
+            Console.ReadKey();
+        }
+        static int Numbers(ref int maxIndex)
+        {
+            int number = 0;
+            int max = 0;
+
+            bool isNumber;
+            string userInput = "";
+            Console.WriteLine("Syötä 10 positiivista lukua. \n ");
+
+            int i = 0;
+            while (i < 10)
+            {
+                Console.Write($"{i + 1}. ");
+                isNumber = int.TryParse(Console.ReadLine(), out number);
+                if (isNumber && number > 0)
+                {
+                    userInput += $"{number} ";
+
+                    if (max < number)
+                    {
+                        max = number;
+                        maxIndex = i + 1;
+                    }
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte. Syötteen täytyy olla positiivinen luku.");
+                }
+            }
+            Console.WriteLine($"\nSyötit seuraavat luvut: {userInput} \n");
+
+            return max;
+        }
+    }
+}
