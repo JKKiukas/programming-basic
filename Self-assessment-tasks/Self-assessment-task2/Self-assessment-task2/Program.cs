3012151ce27e9a15e3bde8fc71c8c89e66edc3ee
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-27 17:02:56 +0300

    Self-assessment task 2 done.

---
+++ Self-assessment-tasks/Self-assessment-task2/Self-assessment-task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Self_assessment_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy käyttäjää syöttämään sanan tai lauseen ja tulostaa sen niin monta kertaa näytölle kuin sanassa on kirjaimia. \n");
+            Console.WriteLine("Syötä sana tai lause. \n");
+
+            string userInput = Console.ReadLine();
+            int letterCount = 1;
+            foreach (char letter in userInput)
+            
+            {
+                letterCount++;
+            }
+            for (int i = 1; i < letterCount; i++)
+            {
+                Console.Write($"\n{i}. ");
+                Console.WriteLine($"{userInput}");
+            }
+        }
+    }
+}
