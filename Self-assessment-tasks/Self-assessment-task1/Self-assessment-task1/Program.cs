a2ad8d21fe895fe82663aa16848119fb8c79f506
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-27 17:02:02 +0300

    Self-assessment task 1 done.

---
+++ Self-assessment-tasks/Self-assessment-task1/Self-assessment-task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Self_assessment_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy käyttäjältä sanan tai lauseen ja tulostaa sen 5 kertaa. \n");
+            Console.WriteLine("Syötä sana tai lause. \n");
+
+            string userInput = Console.ReadLine();
+
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write($"\n{i}. ");
+                Console.WriteLine($"{userInput}");
+            }
+        }
+    }
+}
