15d644ada2594b27e4de89651608b8b5d4cc3dbe
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-10-27 17:03:31 +0300

    Self-assessment task 3 done.

---
+++ Self-assessment-tasks/Self-assessment-task3/Self-assessment-task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Self_assessment_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy käyttäjältä lukuja, kunnes hän syöttää luvun -1. Ohjelma tulostaa näytölle syötettyjen lukujen yhteenlaskun. \n");
+            Console.WriteLine("Syötä luku(ja). \n");
+
+            int add = 0;
+
+            while(true)
+            {
+                bool isNumber = int.TryParse(Console.ReadLine(), out int userInput);
+
+                if (isNumber && userInput != -1)
+                {
+                    add += userInput;
+                }
+                else if (isNumber && userInput == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Syötteen on oltava numero");
+                }
+            }
+            Console.WriteLine($"\nYhteenlaskun tulos on {add}");
+        }
+    }
+}
