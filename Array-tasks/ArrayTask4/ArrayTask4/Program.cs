8e5e3c19e1015d2d619b899a205cad85d8fcd3c3
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-08 13:27:12 +0200

    Array task 4 done.

---
+++ Array-tasks/ArrayTask4/ArrayTask4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 10 yksiulotteista lukua.\n");
+
+            int[] array = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(20);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}. {array[i]:d2}");
+            }
+
+        }
+    }
+}
