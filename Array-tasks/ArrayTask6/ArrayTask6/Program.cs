69274f791beaaca4e233e02fd4cdc8ade19aa44a
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-08 13:00:54 +0200

    Array task 6 done.

---
+++ Array-tasks/ArrayTask6/ArrayTask6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ArrayTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            int[] arrT_1 = RndArray(10);
+            int[] arrT_2 = RndArray(10);
+            int[] arrT_3 = CompareArray(arrT_1, arrT_2 );
+            PrintData(arrT_1, "arrT_1: ");
+            PrintData(arrT_2, "arrT_2: ");
+            PrintData(arrT_3, "arrT_3: ");
+
+        }
+        static void Intro()
+        {
+            Console.WriteLine("        01 02 03 04 05 06 07 08 09 10");
+            Console.WriteLine("        -----------------------------");
+
+        }
+        static int[] RndArray(int lenght)
+        {
+            Random rnd = new Random();
+            int[] arrT = new int[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                arrT[i] = rnd.Next(20);
+            }
+            return arrT;
+        }
+        static void PrintData(int[] arrX, string label)
+        {
+            int[] arrT_1 = new int[10];
+            Console.Write(label);
+            for (int i = 0; i < arrX.Length; i++)
+            {
+                Console.Write($"{arrX[i]:d2} ");
+            }
+            Console.WriteLine();
+        }
+        static int [] CompareArray(int [] arr1, int [] arr2)
+        {
+            int[] arrC = new int[arr1.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] > arr2[i])
+                {
+                    arrC[i] = arr1[i];
+                }
+                else
+                    arrC[i] = arr2[i];
+            }
+            return arrC;
+        }
+        
+    }
+}
