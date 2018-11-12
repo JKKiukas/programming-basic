d7d1af4295e8e15e4e1579554fcaf821687c00f0
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-12 09:04:10 +0200

    String task 1.1 done.

---
+++ String-tasks/String-task1.1/String-task1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String_task1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma ilmoittaa käyttäjän syötteestä merkkien lukumäärän.\n");
+            Console.WriteLine("Anna syöte.\n");
+
+            string text = Console.ReadLine();
+            Console.WriteLine(text.Length);
+        }
+    }
+}
