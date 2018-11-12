8749b484a701fe3ca5faf2a95ac53e8d2f765268
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-12 09:58:06 +0200

    String task 1.3 done.

---
+++ String-tasks/String-task1.3/String task 1.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace String_task_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syötteestä l kirjainten lukumäärän.\n");
+            Console.WriteLine("Anna syöte.\n");
+
+            string text = Console.ReadLine().ToLower();
+            int cntrL = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (text[i] == 'l')
+                    cntrL++;
+            }
+            Console.WriteLine($"\nSyötteessä {text} on {cntrL} L kirjainta.");
+        }
+    }
+}
