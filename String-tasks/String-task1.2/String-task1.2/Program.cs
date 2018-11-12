bf369f9e3bcd09f4f03f7fd180909bb1e0764288
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-12 09:14:40 +0200

    String task 1.2 done.

---
+++ String-tasks/String-task1.2/String-task1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String_task1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa käyttäjän syötteestä e kirjaimen @ merkiksi.\n");
+            Console.WriteLine("Anna syöte.\n");
+
+            string text = Console.ReadLine();
+            Console.WriteLine(text.Replace("e", "@"));
+        }
+    }
+}
