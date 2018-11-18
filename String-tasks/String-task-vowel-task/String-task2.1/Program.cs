3185e134494e16d0f7bd6d7e687a7b7915854aec
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-18 16:38:26 +0200

    String task vowel task done.

---
+++ String-tasks/String-task-vowel-task/String-task2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String_task2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syötteestä vokaalien lukumäärän.\n");
+            Console.WriteLine("Anna syöte.\n");
+
+            string userInput = Console.ReadLine();
+            int vowelCounter = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == 'a' || userInput[i] == 'e' || userInput[i] == 'i' || userInput[i] == 'o' || userInput[i] == 'u' || userInput[i] == 'y' || userInput[i] == 'ä' || userInput[i] == 'ö')
+                {
+                    vowelCounter++;
+                }               
+            }
+            Console.WriteLine($"\nSanassa {userInput} on {vowelCounter} vokaalia.");
+        }
+    }
+}
