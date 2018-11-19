c870cfb369b3715e49e64b54108065a50ae76c51
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-19 09:07:21 +0200

    String task vowel counter version 2 done.

---
+++ String-tasks/String-task-vowel-counter-V2/String-task-vowel-counter-V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace String_task_vowel_counter_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syötteestä vokaalien lukumäärän.\n");
+            string userInput = UserInput();
+            string removedUserInput = VowelsRemove(userInput);
+            Console.WriteLine($"\nVokaaleita syötteessä {userInput} on {userInput.Length - removedUserInput.Length} kappaletta.");
+        }
+
+        static string UserInput()
+        {
+            Console.WriteLine("Syötä sana tai lause.\n");
+            return Console.ReadLine().ToUpper();
+        }
+
+        static string VowelsRemove(string s)
+        {
+            string vowels = "AEIOUYÄÖ";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (s[i] == vowels[j])
+                    {
+                        s = s.Remove(i, 1);
+                        i--;
+                        break;
+                    }
+                }
+            }
+            return s;
+        }
+    }
+}
