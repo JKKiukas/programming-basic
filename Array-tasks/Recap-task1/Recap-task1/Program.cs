c4cfd3e94c2c6397cf774f352363f621353589a1
Jonne Kiukas <jonne.kiukas@hotmail.com>
2018-11-22 08:50:13 +0200

    Recap task 1 version 1 done.

---
+++ Array-tasks/Recap-task1/Recap-task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Recap_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 2, 5, 10, 7, ja 3 tähteä omille riveilleen.\n");
+            int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                PrintStars(numbers[i]);
+            }
+            /*PrintStars(numbers[0]);
+            PrintStars(numbers[1]);
+            PrintStars(numbers[2]);
+            PrintStars(numbers[3]);
+            PrintStars(numbers[4]);*/
+        }
+
+        /// <summary>
+        /// Prints stars
+        /// length = the amount of stars
+        /// </summary>
+        /// <param name="length"></param>
+        static void PrintStars(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
